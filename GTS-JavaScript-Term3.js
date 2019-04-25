5716de9f265a72ca85435a160743331b589e4a73
JayBigDog <44502791+JayBigDog@users.noreply.github.com>
2019-04-25 10:15:23 -0400

    Update GTS-JavaScript-Term3.js

--- GTS-JavaScript-Term3.js
+++ GTS-JavaScript-Term3.js
@@ -1,7 +1,14 @@
+function myFunction() {
+  alert("I am an alert box!");
+}
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
-}
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
