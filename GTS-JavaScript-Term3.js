0b5943092a92cb3f36ffd8b875a19c75bb3e52f0
JayBigDog <44502791+JayBigDog@users.noreply.github.com>
2019-04-28 14:29:45 -0400

    Update GTS-JavaScript-Term3.js
    SlideShow Update (first entry)

--- GTS-JavaScript-Term3.js
+++ GTS-JavaScript-Term3.js
@@ -1,18 +1,36 @@
-<SCRIPT>
-  function myFunction() {
-    alert("I am an alert box!");
-  }
-
   function openForm() {
     document.getElementById("myForm").style.display = "block";
-  }
+
+}
 
   function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm").style.display = "none"
     if (x === "") {
       alert("Name must be filled out");
       return false;
-    }
 
+}
+
+  var i = 0; 
+  var images = [];
+  var time = 1000;
+
+images[0] = 'Images/image1.jpg';
+images[1] = 'Images/GTSportVR.jpg';
+images[2] = 'Images/image3.jpg';
+images[3] = 'Images/image4.jpg';
     
-</SCRIPT>
+  function changeImg(){
+    document.slide.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+  window.onload = changeImg;
+}
