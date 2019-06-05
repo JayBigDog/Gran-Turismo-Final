4b6672ceba5c20e1c3b6baf34901bf5f12a8ce86
Jeremie Masella <44502791+JayBigDog@users.noreply.github.com>
2019-06-05 11:30:36 -0400

    Update GTS-Blog.js

--- GTS-Blog.js
+++ GTS-Blog.js
@@ -5,8 +5,9 @@ function openForm() {
 
 //https://codepen.io/lanesalaman/pen/fgdpI//
 //https://www.dummies.com/programming/java/adding-comments-to-java-code///
-
-  window.onclick = function(e)
+// This code pulls out the html forms and allows the input from the submission box to be redirected and submitted to the submitted area//
+ 
+window.onclick = function(e)
   {   var id =  e.target.id;
    if (id === 'sent')
    { var txt = document.getElementById('example').value
